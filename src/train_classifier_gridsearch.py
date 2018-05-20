3b14904acba39a8c8e9ef5f9b41ea6a55e0bd5a1
Drew Thayer <drewthayer@Drews-MacBook-Pro.local>
2018-05-20 17:50:52 -0600

    add cloud gridsearch script

---
+++ src/train_classifier_gridsearch.py
@@ -0,0 +1,85 @@
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__=='__main__':
+    # load data
+    df = pickle.load( open( 'pkl/aspen_d2_imputemean_alldays.p', 'rb'))
+    # fill na with zero in case any not imputed
+    df.fillna(0, inplace=True)
+
+    ''' N_AVY when case = slab/wet '''
+    cases = [['SLAB','WET'], ['WET','SLAB']]
+    c_true = ['b','g']
+    c_pred = ['r','orange']
+    n_oversamps = [1,1]
+
+    ''' case 1: slab avalanche  '''
+    case = cases[0]
+    data_df = df.copy() # copy to read all columns after dropping
+    print('case: {}'.format(case[0]))
+
+    # drop other binary and probability column
+    c_drop = [c for c in list(df.columns) if case[1] in c]
+    data_df.drop(c_drop, axis=1, inplace=True)
+
+    # train test split in time
+    splitdate = pd.to_datetime('2016-06-01')
+    train_df = data_df[data_df.index <= splitdate]
+    test_df = data_df[data_df.index > splitdate]
+
+    # oversample train data
+    #train_shuffle, counts, factors = oversample(train_df, 'AVY', n=n)
+    #print('oversample to n = {}'.format(n))
+    #pickle.dump( oversamp_df, open( "pkl/aspen_oversamp6.p", "wb" ) )
+    train_shuffle = train_df
+
+    ''' select features and target X,y '''
+    # train set
+    X_train = train_shuffle.copy()
+    y_train = X_train.pop(case[0])
+    # test set
+    X_test = test_df.copy()
+    y_test = X_test.pop(case[0])
+    # datetime for plot
+    test_datetime = pd.to_datetime(X_test.index)
+
+
+    # train model
+
+    param_grid = {
+        'n_estimators': [300, 400, 500, 600],
+        'criterion': ['gini','entropy'],
+        'max_features': ['auto','sqrt','log2'],
+        'min_samples_split': [2, 3, 4, 5],
+        'min_samples_leaf': [1, 2, 3],
+        'oob_score': [True],
+        'n_jobs': [-1]
+        }
+
+    est = RandomForestClassifier()
+
+    grid = GridSearchCV(est, param_grid)
+    grid.fit(X_train, y_train)
+
+    y_hat = grid.predict(X_test)
+
+    score_a = accuracy_score(y_test, y_hat)
+    print('test accuracy = {:0.3f}'.format(score_a))
+
+    score_r = recall_score(y_test, y_hat)
+    print('test recall = {:0.3f}'.format(score_r))
+
+    print(grid.best_params_)
+
+    best_est = grid.best_estimator_
+
+    pickle.dump(best_est, open("best_est.p", "wb"))
